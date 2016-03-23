8f4f0a34892d69d14f525eafb1e1457aa1eb8a8e
Ian Pearce <ian@ianpearce.org>
2016-03-23 11:39:05 -0700

    Check for .mov as well

--- neural_artistic_style.py
+++ neural_artistic_style.py
@@ -107,8 +107,8 @@ def run():
 
     args = parser.parse_args()
 
-    subject_file_extension = os.path.splitext(args.subject)[1]
-    if subject_file_extension == '.mp4':
+    subject_file_extension = os.path.splitext(args.subject)[1].lower()
+    if subject_file_extension == '.mp4' || subject_file_extension == '.mov':
         style_video(args)
     else:
         style_image(args)
